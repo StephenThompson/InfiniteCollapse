4c634ea2bbb479c2fca22b1fa8a3052ffa0007bb
Nigel <munro98@gmail.com>
2017-05-18 21:42:54 +1200

    Added fonts and menu stuff.

---
+++ Assets/Scripts/PlayingCanvas.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+Author: Nigel Munro
+*/
+
+public class PlayingCanvas : MonoBehaviour {
+
+    public GameObject levelTextGameObject;
+    public GameObject scoreTextGameObject;
+    public GameObject gemCountTextGameObject;
+
+    private Text levelText;
+    private Text scoreText;
+    private Text gemCountText;
+
+    // Use this for initialization
+    void Start () {
+        
+        levelText = levelTextGameObject.GetComponent<Text>();
+        levelText.text = "Level " + 999999;
+
+        scoreText = scoreTextGameObject.GetComponent<Text>();
+        scoreText.text = "" + 888888;
+
+        gemCountText = gemCountTextGameObject.GetComponent<Text>();
+        gemCountText.text = "" + 777777;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setLevel(int level)
+    {
+        levelText.text = "Level " + level;
+    }
+
+    public void setScore(int score)
+    {
+        scoreText.text = "" + score;
+    }
+
+    public void setWut(int wut)
+    {
+        gemCountText.text = "" + wut;
+    }
+}
---
+++ Assets/Scripts/ScoreStorer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+Author: Nigel Munro
+*/
+
+public class ScoreStorer : MonoBehaviour {
+
+	const int SCORE_COUNT = 4;
+	int[] scores;
+
+    public GameObject score0;
+    public GameObject score1;
+    public GameObject score2;
+    public GameObject score3;
+
+    //public Text score0Text;
+    //public Text score1Text;
+    //public Text score2Text;
+    //public Text score3Text;
+
+    // Use this for initialization
+    void Start () {
+
+		// Load up Scores
+		scores = new int[SCORE_COUNT];
+
+		for (int i = 0; i < SCORE_COUNT; i++)
+		{
+			if (PlayerPrefs.HasKey(i + "SCORE"))
+			{
+				scores[i] = PlayerPrefs.GetInt(i + "SCORE");
+			}
+			else
+			{
+				scores[i] = 0;
+			}
+			//scores[i] = -i * 2;
+		}
+        //LogScores();
+        //AddScore(200);
+        //AddScore(100);
+        //AddScore(300);
+        //AddScore(4);
+        //LogScores();
+
+
+    }
+
+    // Scores canvas must be Active when called!
+    public void DisplayScores()
+    {
+        Text[] textArray = new Text[SCORE_COUNT];
+        textArray[0] = score0.GetComponent<Text>();
+        textArray[1] = score1.GetComponent<Text>();
+        textArray[2] = score2.GetComponent<Text>();
+        textArray[3] = score3.GetComponent<Text>();
+
+        for (int i = 0; i < SCORE_COUNT; i++)
+        {
+            Text text = textArray[i];
+            text.text = "" + scores[i];
+        }
+    }
+
+	void SaveScores()
+	{
+		for (int i = 0; i < SCORE_COUNT; i++)
+		{
+			PlayerPrefs.SetInt(i + "SCORE", scores[i]);
+		}
+        PlayerPrefs.Save();
+    }
+
+    // Call when the game has ended with new score
+	void AddScore(int score)
+	{
+		for (int i = 0; i < SCORE_COUNT; i++)
+		{
+			if (score > scores[i])
+			{
+				for (int j = SCORE_COUNT - 1; j > i; j--)
+				{
+                    scores[j] = scores[j-1];
+                    
+                }
+				scores[i] = score;
+				return;
+			}
+		}
+        SaveScores();
+    }
+
+	void LogScores () {
+		for (int i = 0; i < SCORE_COUNT; i++)
+		{
+			Debug.Log(scores[i]);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
